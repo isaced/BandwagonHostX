87186675005906d1d40aeb53da0f68ee20b76755
isaced <isaced@163.com>
2016-03-30 10:59:14 +0800

    create project

---
+++ BandwagonHostX/ViewControllers/IndexViewController.swift
@@ -0,0 +1,24 @@
+//
+//  IndexViewController.swift
+//  BandwagonHostX
+//
+//  Created by isaced on 16/3/30.
+//
+//
+
+import UIKit
+
+class IndexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
